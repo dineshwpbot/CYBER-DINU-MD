e2d61ce6c71f5448a66723d56b24680fc84eb206
BLACKY-X-MD <blackyxmd@gmail.com>
2025-02-23 18:27:47 +0530

    Delete plugins/groupHandler.js

--- plugins/groupHandler.js
+++
@@ -1,9 +0,0 @@
-async function handleGroup(sock, groupUpdate) {
-    if (groupUpdate.action === "add") {
-        await sock.sendMessage(groupUpdate.id, { text: "👋 Welcome to the group!" });
-    } else if (groupUpdate.action === "remove") {
-        await sock.sendMessage(groupUpdate.id, { text: "Goodbye! 👋" });
-    }
-}
-
-module.exports = { handleGroup };
