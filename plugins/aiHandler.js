f0fd773abbca15e125d73dc8f06c8cb32eb61e59
BLACKY-X-MD <blackyxmd@gmail.com>
2025-02-23 18:27:22 +0530

    Delete plugins/aiHandler.js

--- plugins/aiHandler.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-
-async function aiChat(sock, sender, query) {
-    const res = await axios.get(`https://api.sometextapi.com/ai?query=${query}`);
-    await sock.sendMessage(sender, { text: res.data.response });
-}
-
-module.exports = { aiChat };
